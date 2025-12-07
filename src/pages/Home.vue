338b4b6165400e6db91c1db0f09a5c4ee75a7784
inhwa <jeoninhwa3@naver.com>
2025-12-08 00:06:38 +0900

    feat: Geolocation 기반 현재 위치 가져오기 로직 추가

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,13 +3,32 @@
 </template>
 
 <script setup>
+	import { onMounted, ref } from "vue";
 	import { weatherApi } from '@/apis'
 
-	const fetchData = async () => {
-		const res = await weatherApi.getCurrentWeather('seoul')
+	const weather = ref(null);
+	const errorMessage = ref('')
 
-		console.log('111', res.data.current)
-	}
+	onMounted(() => {
+		if(!navigator.geolocation) {
+			errorMessage.value = '이 브라우저는 위치 정보를 지원하지 않습니다.'
+			return;
+		}
 
-	fetchData()
+		navigator.geolocation.getCurrentPosition(async (pos) => {
+			const lat = pos.coords.latitude;
+			const lon = pos.coords.longitude;
+			const q = `${lat},${lon}`
+
+			console.log('location', q)
+
+			try {
+				const res = await weatherApi.getCurrentWeather(q)
+				// weather.value = res
+				console.log('res', res.data)
+			} catch (err) {
+				errorMessage.value = '날씨 정보를 불러오는 중 오류가 발생했습니다.'
+			}
+		})
+	})
 </script>
